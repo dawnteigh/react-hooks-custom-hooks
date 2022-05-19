384a3ab40dbf6bcfd5fdced3baea4ea12f779051
Donte Ladatto <donte279@gmail.com>
2022-05-19 15:07:29 -0500

    commit tree ccaad9f08e1897655ecc6f1305d4afba623eea78

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,31 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import About from "./About";
 import ArticleList from "./ArticleList";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import useQuery from "../hooks/useQuery";
 
 function HomePage() {
   // fetch data for posts
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch("http://localhost:4000/posts")
-      .then((r) => r.json())
-      .then((posts) => {
-        setPosts(posts);
-        setIsLoaded(true);
-      });
-  }, []);
+const { data, isLoaded } = useQuery()
+  
 
   // set the document title
-  useEffect(() => {
-    document.title = "Underreacted | Home";
-  }, []);
+ useDocumentTitle("Underreacted | Home");
 
   return (
     <>
       <About />
-      {isLoaded ? <ArticleList posts={posts} /> : <h3>Loading...</h3>}
+      {isLoaded ? <ArticleList posts={data} /> : <h3>Loading...</h3>}
     </>
   );
 }
